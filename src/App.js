be373e198f0a9cc436c7875b8f54d8be5e4b96aa
bojanKalanj <bojan.kalanj@gmail.com>
2019-05-15 14:34:06 +0200

    React Context API

--- src/App.js
+++ src/App.js
@@ -1,41 +1,55 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import Toggle from './ToggleRenderChildrenProps';
 import Portal from "./Portal";
 import Modal from './Modal';
+import { UserContext } from './UserContext';
+import User from './User';
+
+class UserProvider extends Component {
+  state = {
+    id: '1',
+    name: 'Bojan',
+    email: 'bojan@gmail.com'
+  }
+
+  render() {
+    return (
+      <UserContext.Provider value={{ user: this.state }}>
+        {this.props.children}
+      </UserContext.Provider>
+    )
+  }
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <Toggle render={(on, toggle) => {
-          return (
-            <div>
-              {on && <h1>Toggle me</h1>}
-              <button onClick={toggle}>Show / Hide</button>
-            </div>
-          );
-        }}/> */}
-        <Toggle>
-          {(on, toggle) => {
-            return (
-              <Fragment>
-                <button onClick={toggle}>Login</button>
-                <Modal on={on} toggle={toggle}>
-                  <h1>IN MODAL</h1>
-                </Modal>
-              </Fragment>
-            );
-          }}
-        </Toggle>
-        <Portal>
-          <h1>I am Portal</h1>
-        </Portal>
-      </header>
-    </div>
+    <UserProvider>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Toggle>
+            {(on, toggle) => {
+              return (
+                <Fragment>
+                  <button onClick={toggle}>Login</button>
+                  <Modal on={on} toggle={toggle}>
+                    <h1>IN MODAL</h1>
+                  </Modal>
+                </Fragment>
+              );
+            }}
+          </Toggle>
+          <Portal>
+            <h1>I am Portal</h1>
+          </Portal>
+        </header>
+        <User />
+      </div>
+    </UserProvider>
   );
 }
 
---
+++ src/User.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { UserContext } from './UserContext';
+
+export default class User extends Component {
+  render() {
+    return (
+      <UserContext.Consumer>
+        {context => (
+          <div>
+            <h1>{context.user.name}</h1>
+            <h3>{context.user.email}</h3>
+            <h5>{context.user.id}</h5>
+          </div>
+        )}
+      </UserContext.Consumer>
+    );
+  }
+}
